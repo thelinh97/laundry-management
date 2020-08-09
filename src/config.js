c343021ac3e93449444ab2c27ea587c0396ae613
thelinh <vothelinh15@gmail.com>
2020-08-09 21:34:32 +0700

    created config firebase

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBc3MBIlu8oxWmDm5Xdtjk06zuhZf2JMYg",
+  authDomain: "laundry-management-4fefc.firebaseapp.com",
+  databaseURL: "https://laundry-management-4fefc.firebaseio.com",
+  projectId: "laundry-management-4fefc",
+  storageBucket: "laundry-management-4fefc.appspot.com",
+  messagingSenderId: "612824835832",
+  appId: "1:612824835832:web:0f7aeb9d0d0aaa98037d0d",
+  measurementId: "G-0KLTX8XDZY",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
